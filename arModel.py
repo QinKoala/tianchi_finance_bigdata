46db47978a93d1caf511f28b6c313fb010ee67ad
chen xi <claek.chen@icloud.com>
2018-07-05 21:37:14 +0800

    get q

--- arModel.py
+++ arModel.py
@@ -1,11 +1,27 @@
-from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARMA
 import numpy as np
 import pandas as pd
 signal = [1,2,3]
-dates = pd.date_range('20130101', periods=6)
-df = pd.DataFrame([1,2,3,4,5,6], index=dates, columns=list('A'))
-ar_mod = AR(df, freq='D')
-ar_res = ar_mod.fit(2)
-
-res = ar_res.predict('2013-01-04', '2013-01-10')
-print(res)
+dates = pd.date_range('20130101', periods=20)
+df = pd.DataFrame(np.arange(20), index=dates, columns=list('A'))
+def proper_model(data_ts, maxLag):
+    init_bic = 0x7ffffff
+    init_p = 0
+    init_q = 0
+    init_properModel = None
+    for p in np.arange(maxLag):
+        for q in np.arange(maxLag):
+            model = ARMA(data_ts, order=(p, q))
+            try:
+                results_ARMA = model.fit(disp=-1, method='css')
+            except:
+                continue
+            bic = results_ARMA.bic
+            if bic < init_bic:
+                init_p = p
+                init_q = q
+                init_properModel = results_ARMA
+                init_bic = bic
+    return init_bic, init_p, init_q, init_properModel
+print(df)
+print(proper_model(df, 7))
